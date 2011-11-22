5ea5b05ce4ddc75c81ae857bdf8db8a6e7dbf383
Wouter Roosendaal <tuvokki@gmail.com>
2011-11-22 22:04:38 +0100

    Added logging

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -10,7 +10,11 @@ public class Bootstrap extends Job {
 	public void doJob() {
 		// Check if the database is empty
 		if (Blogger.count() == 0) {
+			Logger.info("Loading initial blog data ...");
 			Fixtures.loadModels("initial-blog-data.yml");
+		}
+		if (Game.count() == 0) {
+			Logger.info("Loading initial game data ...");
 			Fixtures.loadModels("initial-game-data.yml");
 		}
 	}
